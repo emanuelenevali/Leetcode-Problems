f885d407b82bcb42223888f0ed3cf9b6c1faf48e
Emanuele Nevali <manu4375@gmail.com>
2022-10-08 15:49:01 +0200

    ex 32 in O(N^2) so bad

---
+++ Ex32/main.c
@@ -0,0 +1,40 @@
+//Inefficient solution O(N^2)
+#include <string.h>
+#include <stdio.h>
+int longestValidParentheses(char *s){
+    int best_len = 0;
+    int count_open;
+    int count_close;
+    for(int i=0; i<strlen(s); i++){       
+        int len = 0;
+        count_open = 0;
+        count_close = 0;
+        if (s[i] == '(')
+            count_open++;
+        else
+            continue;
+        for(int j=i+1; j<strlen(s); j++){
+            printf("open: %d,close: %d\n", count_open, count_close);
+            if(count_close>count_open){
+                len--;
+                break;}
+            else{
+                if (s[i] == '('){
+                    count_open++;
+                    len++;}
+                else{
+                    count_close++;
+                    len++;}
+            }
+        }
+        if(len>best_len)
+            best_len=len;
+    }
+    return best_len;
+}
+
+int main(void){
+    char* s = "(()()()()))";
+    printf("\n\n\n%d", longestValidParentheses(s));
+    return 0;
+}
