2f119b057b84fdc4306144f6172673968819632f
Emanuele Nevali <manu4375@gmail.com>
2022-10-14 21:17:06 +0200

    ex 27

--- Ex27/Ex27.c
+++ Ex27/Ex27.c
@@ -10,6 +10,18 @@ int removeDuplicates(int* nums, int numsSize){
     return k;}
 
 
+int removeElement(int* nums, int numsSize, int val){
+    int k = 0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=val){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+
 
 
 
