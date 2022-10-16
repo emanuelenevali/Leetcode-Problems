ee08e91426021c28b87ddd0067c4ff20c5adb864
Emanuele Nevali <manu4375@gmail.com>
2022-10-17 00:33:51 +0200

    Ex20

--- Ex20/Ex20.py
+++ Ex20/Ex20.py
@@ -1,21 +1,14 @@
-class Solution(object):
-    @classmethod
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        temp_list = []
-        old_number = None
-        for i in nums:
-            if i != old_number:
-                temp_list.append(i)
-                old_number = i
-            else:
-                continue
-        for i in range(len(temp_list)):
-            nums[i] = temp_list[i]
-        return len(temp_list)
+class Solution:
+    def isValid(self, s):
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
 
-nums = [1, 1, 2]
-print(nums[:Solution.removeDuplicates(nums)])
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
