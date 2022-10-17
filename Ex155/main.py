0beca0215d1bbba74beee15f33322a73efae3019
Emanuele Nevali <manu4375@gmail.com>
2022-10-17 22:19:03 +0200

    Ex 155

--- Ex155/main.py
+++ Ex155/main.py
@@ -1,36 +1,19 @@
-class MinStack(object):
-
+class MinStack:
     def __init__(self):
-        self.stack=[]
-        
+        self.stack = []
+        self.minStack = []
 
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
+    def push(self, val: int) -> None:
         self.stack.append(val)
-        
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
-    def pop(self):
-        """
-        :rtype: None
-        """
+    def pop(self) -> None:
         self.stack.pop()
+        self.minStack.pop()
 
-    def top(self):
-        """
-        :rtype: int
-        """
+    def top(self) -> int:
         return self.stack[-1]
-        
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        min=self.stack[0]
-        for i in range(len(self.stack)):
-            if self.stack[i]<min:
-                min=self.stack[i]
-        return min
+    def getMin(self) -> int:
+        return self.minStack[-1]
