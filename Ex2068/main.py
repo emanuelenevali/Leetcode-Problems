22197021685cc0d073574653b0a5cc76f333e4dd
Emanuele Nevali <manu4375@gmail.com>
2022-10-28 22:33:10 +0200

    Ex2068

---
+++ Ex2068/main.py
@@ -0,0 +1,3 @@
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        return all(v < 4 for v in ((Counter(word1) - Counter(word2)) + (Counter(word2) - Counter(word1))).values())
